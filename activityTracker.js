eefb5164a71bdedeee85e9aa707effc1a5f78df9
Baptiste Williams <tc1028@hotmail.com>
2025-07-08 20:18:35 -0700

    added weekly data with real activities

---
+++ activityTracker.js
@@ -0,0 +1,9 @@
+const myWeek = [
+  { day: "Monday", activity: "Doing School Work", category: "academic", hoursSpent: 2, enjoyment: 6, timeOfDay: "afternoon" },
+  { day: "Tuesday", activity: "Working Out", category: "physical", hoursSpent: 1, enjoyment: 7, timeOfDay: "morning" },
+  { day: "Wednesday", activity: "Going to Work", category: "professional", hoursSpent: 4, enjoyment: 5, timeOfDay: "morning" },
+  { day: "Thursday", activity: "Going to Meetings", category: "professional", hoursSpent: 2, enjoyment: 4, timeOfDay: "afternoon" },
+  { day: "Friday", activity: "Playing Video Games", category: "leisure", hoursSpent: 3, enjoyment: 9, timeOfDay: "evening" },
+  { day: "Saturday", activity: "Working Out", category: "physical", hoursSpent: 1.5, enjoyment: 8, timeOfDay: "morning" },
+  { day: "Sunday", activity: "Doing School Work", category: "academic", hoursSpent: 2.5, enjoyment: 7, timeOfDay: "evening" }
+];
